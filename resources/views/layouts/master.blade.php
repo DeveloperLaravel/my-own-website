948737d5ae3eb57fbd3f70448d27731e21dfcf7e
Developerlaravel <hnarfr20063@github.com>
2025-07-05 21:46:04 +0200

    edit file

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -25,11 +25,12 @@
 
   <!-- Vendor CSS Files -->
   <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-
   <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
   <link href="{{ asset('assets/vendor/aos/aos.css')}}" rel="stylesheet">
-  <link href="{{ asset('assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
-   <link href=" {{ asset('assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+
+  
+  <!-- <link href="{{ asset('assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet"> -->
+   <!-- <link href=" {{ asset('assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet"> -->
 
   <!-- Main CSS File -->
   <link href="{{ asset('assets/css/main.css')}}" rel="stylesheet">
@@ -51,14 +52,14 @@
   <div id="preloader"></div>
 
   <!-- Vendor JS Files -->
-  <!-- <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script> -->
-  <script src="{{ asset('assets/vendor/php-email-form/validate.js')}}"></script>
+  <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
   <script src="{{ asset('assets/vendor/aos/aos.js')}}"></script>
-  <script src="{{ asset('assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
-  <script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
-  <script src="{{ asset('assets/vendor/purecounter/purecounter_vanilla.js')}}"></script>
-  <script src="{{ asset('assets/vendor/imagesloaded/imagesloaded.pkgd.min.js')}}"></script>
-  <script src="{{ asset('assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+  <!-- <script src="{{ asset('assets/vendor/purecounter/purecounter_vanilla.js')}}"></script> -->
+  <!-- <script src="{{ asset('assets/vendor/imagesloaded/imagesloaded.pkgd.min.js')}}"></script> -->
+  <!-- <script src="{{ asset('assets/vendor/php-email-form/validate.js')}}"></script> -->
+  <!-- <script src="{{ asset('assets/vendor/glightbox/js/glightbox.min.js')}}"></script> -->
+  <!-- <script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js')}}"></script> -->
+  <!-- <script src="{{ asset('assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script> -->
 
   <!-- Main JS File -->
   <script src="{{ asset('assets/js/main.js')}}"></script>
